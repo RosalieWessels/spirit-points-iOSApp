9fa957a4eae0c220240e3111803b36a18518946e
RosalieWessels <rosalie.wessels@yahoo.com>
2020-11-23 16:17:04 -0800

    Initial Commit

---
+++ Shared/SpiritPointsiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiritPointsiOSAppApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SpiritPointsiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
