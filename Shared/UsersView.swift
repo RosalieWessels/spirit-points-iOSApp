1d2d2d38652cab86241b8ca110f1582963220ab5
RosalieWessels <rosalie.wessels@yahoo.com>
2021-05-01 17:57:58 -0700

    Started working on see users screen

--- Shared/UsersView.swift
+++ Shared/UsersView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct UsersView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Manage Users:")
+                .font(.system(.title2,design: .rounded)).fontWeight(.bold).padding()
+            VStack {
+                Text("jbruno@pinewood.edu")
+                Text("hmark@pinewood.edu")
+                Text("glemmon@pinewood.edu")
+                Text("etyson@pinewood.edu")
+                Text("23rwessels@pinewood.edu")
+                Text("22mpistelak@pinewood.edu")
+            }
+            VStack {
+                Text("22awong@pinewood.edu")
+                Text("22akamanger@pinewood.edu")
+                Text("21vreed@pinwood.edu")
+                Text("21sking@pinewood.edu")
+                Text("21mrodriguezsteube@pinewood.edu")
+                Text("21alevy@pinewood.edu")
+            }
+            
+            Text("Delete my account")
+                .font(.system(.title2,design: .rounded)).fontWeight(.bold).padding()
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Delete")
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .background(Color.red)
+            .cornerRadius(10)
+        }
     }
 }
 
