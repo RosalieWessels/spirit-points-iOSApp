c665f7d95ba31245acb8206c234a4c4fd26ec0a8
RosalieWessels <rosalie.wessels@yahoo.com>
2021-05-19 14:12:59 -0700

    added ability to see history data from Firebase

--- Shared/HistoryView.swift
+++ Shared/HistoryView.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct HistoryView: View {
+    @State var historyList = [historyItem(stringDate: "test", reason: "test", user: "test", grade: 0, points: 0)]
+    @State var db = Firestore.firestore()
+    
     var body: some View {
         ScrollView{
             VStack {
@@ -15,11 +19,54 @@ struct HistoryView: View {
                         .foregroundColor(.green)
                         .font(.system(.largeTitle, design: .rounded))
                     .fontWeight(.black).padding()
-                Text("Date: Fri Mar 19 2021 14:53:22 GMT-0700 (PDT) Reason: Green and gold spirit day Gold cohort User: 22akamangar@pinewood.edu Grade: 10 Points: 4").font(.headline)
+                
+                if historyList[0].stringDate != "test" {
+                    ForEach(historyList) { result in
+                        
+                        Text("Date: \(result.stringDate), Reason: \(result.reason), User: \(result.user), Grade: \(result.grade), Points: \(result.points)").font(.headline)
+                        
+                        Spacer()
+                                .frame(height: 20)
+                    }
+                    
+
+                }
         }
         
         }
+        .onAppear(perform: {
+            getHistory()
+        })
         
+    }
+    
+    func getHistory() {
+        db.collection("history")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                        if let stringDate = document.get("stringDate") as? String {
+                            if let reason = document.get("reason") as? String {
+                                if let user = document.get("user") as? String {
+                                    if let grade = document.get("grade") as? Int{
+                                        if let points = document.get("points") as? Int {
+                                            let newHistoryItem = historyItem(stringDate: stringDate, reason: reason, user: user, grade: grade, points: points)
+                                            
+                                            historyList.append(newHistoryItem)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if historyList.count >= 2 {
+                        historyList.remove(at: 0)
+                    }
+                }
+        }
     }
 }
 
@@ -27,4 +74,13 @@ struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
         HistoryView()
     }
+}
+
+struct historyItem: Identifiable {
+    var id = UUID()
+    var stringDate : String
+    var reason : String
+    var user : String
+    var grade : Int
+    var points : Int
 }
